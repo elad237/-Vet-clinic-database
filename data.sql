598209ac8bb08a82cae85ff95f9c5ebee84c1ede
elad237 <tumbu.elad@gmail.com>
2023-06-10 18:41:36 +0100

    Updated the schema.sql and data.sql and queries.sql files

--- data.sql
+++ data.sql
@@ -4,7 +4,7 @@ INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
 VALUES ('Agumon', '2020-02-03', 0, '1', 10.23);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
-VALUES ('Gabumon', '2018-11-15', 2, '1', 8);
+VALUES ('Gabumon', '2018-11-15', 2, '1', 8).0;
 
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
 VALUES ('Pikachu', '2021-01-07', 1, '0', 15.04);
@@ -13,7 +13,7 @@ INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
 VALUES ('Devimon', '2017-05-12', 5, '1', 11);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
-VALUES ('Charmander', '2020-02-08', 0, '0', -11);
+VALUES ('Charmander', '2020-02-08', 0, '0', -11.0);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
 VALUES ('Plantmon', '2021-11-15', 2, '1', -5.7);
@@ -22,16 +22,16 @@ INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
 VALUES ('Squirtle', '1993-04-02', 3, '0', -12.13);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
-VALUES ('Angemon', '2005-06-12', 1, '1', -45);
+VALUES ('Angemon', '2005-06-12', 1, '1', -45.0);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
 VALUES ('Boarmon', '2005-06-07', 7, '1', 20.4);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
-VALUES ('Blossom', '1998-10-13', 3, '1', 17);
+VALUES ('Blossom', '1998-10-13', 3, '1', 17.0);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg)
-VALUES ('Ditto', '2022-05-14', 4, '1', 22);
+VALUES ('Ditto', '2022-05-14', 4, '1', 22.0);
 
 
 -- day3
